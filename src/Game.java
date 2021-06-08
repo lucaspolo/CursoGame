53236eab32ecb3c7364e7efabe2f501e17ae9906
Lucas <Polo>
2021-06-07 21:52:20 -0300

    Capitulo 11 - Os retoques finais
    Criado sistema de pontuação e game over do jogo, adicionado menu de pause com opções de continuar ou sair do jogo, adicionado sons do jogo.

--- src/Game.java
+++ src/Game.java
@@ -26,6 +26,8 @@ public class Game extends JPanel {
 
     private BufferedImage bg;
 
+    private BufferedImage splashLogo;
+
     public Game() {
         addKeyListener(new KeyListener() {
             @Override
@@ -33,29 +35,64 @@ public class Game extends JPanel {
 
             }
 
-            private void changeKeys(KeyEvent keyEvent, boolean value) {
-                switch (keyEvent.getKeyCode()) {
-                    case KeyEvent.VK_UP -> kCima = value;
-                    case KeyEvent.VK_DOWN -> kBaixo = value;
-                    case KeyEvent.VK_LEFT -> kEsquerda = value;
-                    case KeyEvent.VK_RIGHT -> kDireita = value;
-                    case KeyEvent.VK_SHIFT -> kShifit = value;
-                }
-            }
-
             @Override
             public void keyPressed(KeyEvent keyEvent) {
                 if(estado == 'E') {
-                    this.changeKeys(keyEvent, true);
+                    switch (keyEvent.getKeyCode()) {
+                        case KeyEvent.VK_UP -> kCima = true;
+                        case KeyEvent.VK_DOWN -> kBaixo = true;
+                        case KeyEvent.VK_LEFT -> kEsquerda = true;
+                        case KeyEvent.VK_RIGHT -> kDireita = true;
+                        case KeyEvent.VK_SHIFT -> kShifit = true;
+                        case KeyEvent.VK_ESCAPE -> {
+                            estado = 'P';
+                            Recursos.getInstance().tocarSomMenu();
+                        }
+                    }
                 } else if(estado == 'P') {
-
+                    switch (keyEvent.getKeyCode()) {
+                        case KeyEvent.VK_UP -> {
+                            Recursos.getInstance().pauseOpt = 0;
+                            Recursos.getInstance().tocarSomMenu();
+                        }
+                        case KeyEvent.VK_DOWN -> {
+                            Recursos.getInstance().pauseOpt = 1;
+                            Recursos.getInstance().tocarSomMenu();
+                        }
+                        case KeyEvent.VK_ENTER -> {
+                            Recursos.getInstance().tocarSomMenu();
+                            if(Recursos.getInstance().pauseOpt == 0) {
+                                estado = 'E';
+                                kCima = false;
+                                kBaixo = false;
+                                kEsquerda = false;
+                                kDireita = false;
+                            } else {
+                                System.exit(0);
+                            }
+                        }
+                        case KeyEvent.VK_ESCAPE -> {
+                            System.out.println("Saindo da pausa");
+                            estado = 'E';
+                            kCima = false;
+                            kBaixo = false;
+                            kEsquerda = false;
+                            kDireita = false;
+                        }
+                    }
                 }
             }
 
             @Override
             public void keyReleased(KeyEvent keyEvent) {
                 if(estado == 'E') {
-                    this.changeKeys(keyEvent, false);
+                    switch (keyEvent.getKeyCode()) {
+                        case KeyEvent.VK_UP -> kCima = false;
+                        case KeyEvent.VK_DOWN -> kBaixo = false;
+                        case KeyEvent.VK_LEFT -> kEsquerda = false;
+                        case KeyEvent.VK_RIGHT -> kDireita = false;
+                        case KeyEvent.VK_SHIFT -> kShifit = false;
+                    }
                 }
             }
         });
@@ -69,6 +106,7 @@ public class Game extends JPanel {
 
         try {
             bg = ImageIO.read(Objects.requireNonNull(getClass().getResource("imgs/bg.png")));
+            splashLogo = ImageIO.read(getClass().getResource("imgs/logo.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -89,11 +127,13 @@ public class Game extends JPanel {
         super.paintComponent(g2d);
 
         if(estado == 'S') {
-            g2d.setColor(Color.WHITE);
-            g2d.fillRect(0, 0, Principal.LARGURA_TELA, Principal.ALTURA_TELA);
+            g2d.drawImage(splashLogo, 0, 0, null);
         } else if(estado == 'R') {
             g2d.setColor(Color.BLACK);
             g2d.fillRect(0, 0, Principal.LARGURA_TELA, Principal.ALTURA_TELA);
+            g2d.setFont(Recursos.getInstance().fontMenu);
+            g2d.setColor(Color.WHITE);
+            g2d.drawString(Recursos.getInstance().msgFim, 150, 200);
         } else {
             g2d.drawImage(bg, 0, 0, Principal.LARGURA_TELA, Principal.ALTURA_TELA, null);
             g2d.setColor(Color.GRAY);
@@ -104,10 +144,24 @@ public class Game extends JPanel {
             g2d.drawImage(bolinha.img, bolinha.af, null);
 
             if(estado == 'E') {
-
+                g2d.setFont(Recursos.getInstance().fontPontuacao);
+                g2d.setColor(Color.WHITE);
+                g2d.drawString(Recursos.getInstance().pontosInimigo + "pts", 120, 40);
+                g2d.drawString(Recursos.getInstance().pontosJogador + "pts", 460, 40);
             } else {
                 g2d.setColor(new Color( 0, 0, 0, 128));
                 g2d.fillRect(0, 0, Principal.LARGURA_TELA, Principal.ALTURA_TELA);
+                g2d.setFont(Recursos.getInstance().fontMenu);
+                g2d.setColor(Color.WHITE);
+                g2d.drawString("JOGO PAUSADO", 150, 80);
+                g2d.drawString("Continuar", 220, 200);
+                g2d.drawString("Sair", 220, 270);
+
+                if (Recursos.getInstance().pauseOpt == 0) {
+                    g2d.fillRect(180, 170, 30, 30);
+                } else {
+                    g2d.fillRect(180, 240, 30, 30);
+                }
             }
         }
     }
@@ -145,8 +199,11 @@ public class Game extends JPanel {
             inimigo.update(deltaTime);
             bolinha.update(deltaTime);
             testeColisoes(deltaTime);
+            testeFimJogo();
         } else if(estado == 'G') {
             estado = 'R';
+            reiniciar();
+            agendarTransicao(2000, 'E');
         }
     }
 
@@ -169,22 +226,26 @@ public class Game extends JPanel {
 
         if(bolinha.posX + (bolinha.raio * 2) >= Principal.LARGURA_TELA) {
             bolinha.velX *= -1;
-            bolinha.posX = Principal.LARGURA_TELA - (bolinha.raio * 2);
+            bolinha.posX = Principal.LARGURA_TELA / 2 - bolinha.raio + 90;
+            Recursos.getInstance().pontosInimigo++;
         }
 
         if(bolinha.posX <= 0) {
             bolinha.velX *= -1;
-            bolinha.posX = 0;
+            bolinha.posX = Principal.LARGURA_TELA / 2.0 - bolinha.raio - 90;
+            Recursos.getInstance().pontosJogador++;
         }
 
         if(bolinha.posY + (bolinha.raio * 2) >= Principal.ALTURA_TELA) {
             bolinha.velY *= -1;
             bolinha.posY = Principal.ALTURA_TELA - (bolinha.raio * 2);
+            Recursos.getInstance().tocarSomBolinha();
         }
 
         if(bolinha.posY <= 0) {
             bolinha.velY *= -1;
             bolinha.posY = 0;
+            Recursos.getInstance().tocarSomBolinha();
         }
 
         var ladoHorizontal = jogador.getCentroX() - bolinha.getCentroX();
@@ -199,6 +260,7 @@ public class Game extends JPanel {
             var seno = ladoVertical / hipotenusa;
             bolinha.velX = (-bolinha.velocidadeBase) * cosseno;
             bolinha.velY = (-bolinha.velocidadeBase) * seno;
+            Recursos.getInstance().tocarSomBolinha();
         }
 
         ladoHorizontal = inimigo.getCentroX() - bolinha.getCentroX();
@@ -213,6 +275,7 @@ public class Game extends JPanel {
             var seno = ladoVertical / hipotenusa;
             bolinha.velX = (-bolinha.velocidadeBase) * cosseno;
             bolinha.velY = (-bolinha.velocidadeBase) * seno;
+            Recursos.getInstance().tocarSomBolinha();
         }
 
         if(inimigo.posY <= 0 || inimigo.posY + (inimigo.raio * 2) >= Principal.ALTURA_TELA) {
@@ -234,5 +297,36 @@ public class Game extends JPanel {
             }
         });
         thread.start();
+    }
+
+    public void testeFimJogo() {
+        if(Recursos.getInstance().pontosJogador == Recursos.getInstance().maxPontos) {
+            Recursos.getInstance().msgFim = "VOCÊ VENCEU!";
+            estado = 'G';
+        } else if(Recursos.getInstance().pontosInimigo == Recursos.getInstance().maxPontos) {
+            Recursos.getInstance().msgFim = "VOCÊ PERDEU!";
+            estado = 'G';
+        }
+    }
+
+    public void reiniciar() {
+        inimigo.posX = (Principal.LARGURA_TELA * (1.0 / 8.0) - inimigo.raio);
+        inimigo.posY = (Principal.ALTURA_TELA / 2.0) - inimigo.raio;
+        inimigo.velY = inimigo.velocidadeBase;
+        Recursos.getInstance().pontosInimigo = 0;
+
+        jogador.posX = (Principal.LARGURA_TELA * (7.0 / 8.0) - jogador.raio);
+        jogador.posY = (Principal.ALTURA_TELA / 2.0) - jogador.raio;
+        Recursos.getInstance().pontosJogador = 0;
+
+        bolinha.velX = bolinha.velocidadeBase / 2;
+        bolinha.velY = bolinha.velocidadeBase / 2;
+        bolinha.posX = (Principal.LARGURA_TELA / 2.0) - bolinha.raio;
+        bolinha.posX = (Principal.ALTURA_TELA / 2.0) - bolinha.raio;
+
+        kCima = false;
+        kBaixo = false;
+        kEsquerda = false;
+        kDireita = false;
     }
 }
--- src/Recursos.java
+++ src/Recursos.java
@@ -1,9 +1,35 @@
+import javax.sound.sampled.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Recursos {
     private static Recursos singleton = null;
-    private Recursos() {
+    public int pontosJogador;
+    public int pontosInimigo;
+    public Font fontPontuacao;
+    public int maxPontos;
+    public Font fontMenu;
+    public String msgFim;
+    public int pauseOpt;
 
+    public AudioInputStream menu;
+    public AudioInputStream silence;
+    public AudioInputStream set;
+    public Clip clipMenu;
+    public Clip clipSilence;
+    public Clip clipSet;
+
+    private Recursos() {
+        pontosInimigo = 0;
+        pontosJogador = 0;
+        fontPontuacao = new Font("Arial narrow", Font.LAYOUT_LEFT_TO_RIGHT, 40);
+        maxPontos = 5;
+        fontMenu = new Font("Arial narrow", Font.CENTER_BASELINE, 50);
+        msgFim = "";
+        pauseOpt = 0;
+
+        carregarSons();
     }
 
     public static Recursos getInstance() {
@@ -18,5 +44,39 @@ public class Recursos {
         int largura = x2 - x1;
         int altura = y2 - y1;
         return img.getSubimage(x1, y1, largura, altura);
+    }
+
+    public void carregarSons() {
+        try {
+            menu = AudioSystem.getAudioInputStream(getClass().getResource("audio/menu.wav"));
+            silence = AudioSystem.getAudioInputStream(getClass().getResource("audio/silence.wav"));
+            set = AudioSystem.getAudioInputStream(getClass().getResource("audio/set.wav"));
+
+            clipMenu = AudioSystem.getClip();
+            clipSilence = AudioSystem.getClip();
+            clipSet = AudioSystem.getClip();
+
+            clipMenu.open(menu);
+            clipSilence.open(silence);
+            clipSet.open(set);
+
+            clipSilence.start();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void tocarSomBolinha() {
+        clipSet.setFramePosition(0);
+        clipSet.start();
+    }
+
+    public void tocarSomMenu() {
+        clipMenu.setFramePosition(0);
+        clipMenu.start();
     }
 }
